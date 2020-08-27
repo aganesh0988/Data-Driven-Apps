7b61ea1cdecebba0f15a864e48ee9257c28257b3
Andrea <aganesh0988@gmail.com>
2020-08-27 17:06:45 -0500

    Part-3-COMPLETED

--- routes.js
+++ routes.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = require('./app');
+const db = require('./db/models');
 // const morgan = require('morgan');
 
 const router = express.Router();
 // router.use(morgan('dev'));
 
 
-router.get('/', (req, res) => {
-  throw new Error('This is a test error!');
-  res.render('index', { title: 'Home' });
+router.get('/', async (req, res, next) => {
+  try {
+    const books = await db.Book.findAll({ order: [['title', 'ASC']] });
+    res.render('index', { title: 'Home', books });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
