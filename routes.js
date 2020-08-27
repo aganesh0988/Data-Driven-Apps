70dc8e5100ffd098c1ee46768051ff72c42d1a4c
Bonnie Hardie <bonniemariehardie@gmail.com>
2020-08-27 16:34:11 -0400

    completed part one

--- routes.js
+++ routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const app = require('./app');
+// const morgan = require('morgan');
 
 const router = express.Router();
+// router.use(morgan('dev'));
+
 
 router.get('/', (req, res) => {
+  throw new Error('This is a test error!');
   res.render('index', { title: 'Home' });
 });
 
