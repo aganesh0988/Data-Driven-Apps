30caa7066cb0b37b4fc241382c3016c8486cdf95
Andrea <aganesh0988@gmail.com>
2020-08-27 17:57:55 -0500

    changed pug file

---
+++ README.md
@@ -0,0 +1,3 @@
+Need to install:
+
+npm install and create the database, user, and table. Also install env.
--- routes.js
+++ routes.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const app = require('./app');
 const db = require('./db/models');
-const { values } = require('sequelize/types/lib/operators');
+//const { values } = require('sequelize/types/lib/operators');
 // const morgan = require('morgan');
 
 const router = express.Router();
 // router.use(morgan('dev'));
 
 
-router.get('/', async (req, res, next) => {
-  try {
-    const books = await db.Book.findAll({ order: [['title', 'ASC']] });
-    res.render('book-list', { title: 'Books', books });
-  } catch (err) {
-    next(err);
-  }
-});
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await db.Book.findAll({ order: [['title', 'ASC']] });
+  res.render('book-list', { title: 'Books', books });
+}));
+
 
 module.exports = router;
